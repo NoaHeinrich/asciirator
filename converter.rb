944e32c134288906eb166985f7ec0aa46611de3a
NoahHeinrich <noawriteplay@gmail.com>
2019-11-01 15:43:31 -0500

    Fixed the color

--- converter.rb
+++ converter.rb
@@ -29,18 +29,20 @@ class Converter
   def map_brightness
     @pixels.map do |row|
       row.map do |pixel|
-        (pixel[0] + pixel[1] + pixel[2]) / 3
+        # (pixel[0] + pixel[1] + pixel[2]) / 3
+        (0.299*pixel[0] + 0.585*pixel[1] + 0.114*pixel[2])
       end
     end
   end
 
   def map_ascii
     chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+    chars.reverse!
     @brightness_matrix.each_with_index.map do |row, row_index|
       pixel_row = @pixels[row_index]
       row.each_with_index.map do |cell, cell_index|
         pixel_cell = pixel_row[cell_index]
-        Rainbow((chars[cell/4]) * 3).color(pixel_cell[0], pixel_cell[1], pixel_cell[2])
+        Rainbow((chars[cell/4]) * 3).color(pixel_cell[0], pixel_cell[1], pixel_cell[2]).bright
       end
     end
   end
