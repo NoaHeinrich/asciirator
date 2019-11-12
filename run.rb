cc1739ccd935a1a3a43c95d2dfc705d1f70e157a
Noah Heinrich <noahheinrich@gmail.com>
2019-11-12 14:10:02 -0600

    Added thor interface

--- run.rb
+++ run.rb
@@ -1,19 +1,31 @@
 require "./converter"
+require 'thor'
 
-def run
-  input = "y"
-  while input == "y"
-    puts "Welcome to ASCIIrator!"
-    puts "Plese type in an image url:"
-    img = gets.chomp
-    converter = Converter.new(img)
+class ASCIIrator < Thor
+
+  desc "asciirate", "converts an image url into ascii art"
+  def asciirate(url)
+    converter = Converter.new(url)
     puts "We are about to print your art! You might want to zoom out!"
     sleep(3)
     converter.display
-    puts "Would you like to go again? (Y/n)"
-    input = gets.chomp.downcase
   end
-
 end
+ASCIIrator.start(ARGV)
+# def run
+#   input = "y"
+#   while input == "y"
+#     puts "Welcome to ASCIIrator!"
+#     puts "Plese type in an image url:"
+#     img = gets.chomp
+#     converter = Converter.new(img)
+#     puts "We are about to print your art! You might want to zoom out!"
+#     sleep(3)
+#     converter.display
+#     puts "Would you like to go again? (Y/n)"
+#     input = gets.chomp.downcase
+#   end
 
-run
+# end
+
+# run
